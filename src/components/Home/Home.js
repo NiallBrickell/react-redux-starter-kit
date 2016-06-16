2d6b227f054230e3cb99adac899f3cdef23f58d8
Niall Brickell <niallbrickell@gmail.com>
2016-06-16 21:39:06 +0100

    Fixed tests.
    Still ugly - needs to be fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Home = () => (
 	<div>
-		<h1>React-Redux Starter Kit!</h1>
+		<h1>Welcome!</h1>
 	</div>
 );
 
--- tests/components/Home/Home.spec.js
+++ tests/components/Home/Home.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HomeView } from 'components/Home/Home'
+import { Home } from 'components/Home/Home'
 import { render } from 'enzyme'
 
 describe('(View) Home', () => {
@@ -10,15 +10,9 @@ describe('(View) Home', () => {
   })
 
   it('Renders a welcome message', () => {
-    const welcome = _component.find('h4')
+    const welcome = _component.find('h1')
     expect(welcome).to.exist
     expect(welcome.text()).to.match(/Welcome!/)
-  })
-
-  it('Renders an awesome duck image', () => {
-    const duck = _component.find('img')
-    expect(duck).to.exist
-    expect(duck.attr('alt')).to.match(/This is a duck, because Redux!/)
   })
 
 })
